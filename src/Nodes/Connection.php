650bb44e4c1dd2034a9f7eb53e20b1f78847149e
Randy Glenn Aguirre <rgat.randy@gmail.com>
2015-10-24 10:40:15 +0800

    implement connection

--- src/Nodes/Connection.php
+++ src/Nodes/Connection.php
@@ -2,14 +2,55 @@
 
 namespace ElasticEqb\Nodes;
 
+
+use GuzzleHttp\Client;
+
 class Connection
 {
+    protected $version;
+    protected $client;
     protected $config;
-    protected $host;
-    protected $post;
 
     public function __construct()
     {
-//        $this->host = config('')
+        $this->config = config('elastic');
+        $this->setVersion($this->config['version']);
+        $this->setClient(new Client([
+            'base_uri' => ($this->config['ssl']) ? 'https://' : 'http://' . $this->config['host'] . ':' . $this->config['port']
+        ]));
     }
+
+    /**
+     * @return mixed
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
+
+    /**
+     * @param mixed $version
+     */
+    public function setVersion($version)
+    {
+        $this->version = $version;
+    }
+
+    /**
+     * @return \GuzzleHttp\Client
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param \GuzzleHttp\Client $client
+     */
+    public function setClient(Client $client)
+    {
+        $this->client = $client;
+    }
+
+
 }
